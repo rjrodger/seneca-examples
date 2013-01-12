ac4989074b0ddac484ab4907217a678c91b65827
Richard Rodger <richard@ricebridge.com>
2013-01-12 03:16:07 +0000

    admin

--- user-accounts/app.js
+++ user-accounts/app.js
@@ -12,7 +12,6 @@ var conf = {
   port: argv.p || 80
 }
 
-
 var si = seneca()
 si.use('util')
 si.use('user')
@@ -22,7 +21,6 @@ si.use('auth',{redirect:{login:{win:'/account',fail:'/login#failed'}}})
 var app = express()
 app.enable('trust proxy')
 
-//app.use(express.logger())
 app.use(express.cookieParser())
 app.use(express.query())
 app.use(express.bodyParser())
@@ -56,6 +54,9 @@ app.get('/account', function(req, res){
 
 var u = si.pin({role:'user',cmd:'*'})
 u.register({nick:'u1',name:'nu1',email:'u1@example.com',password:'u1',active:true})
+u.register({nick:'u2',name:'nu2',email:'u2@example.com',password:'u2',active:true})
+u.register({nick:'a1',name:'na1',email:'a1@example.com',password:'a1',active:true,admin:true})
+
 
 
 var server = http.createServer(app)
