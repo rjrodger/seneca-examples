064b5195bb25be6a081cb6b3f3c1fa46e2ebb6ad
Richard Rodger <richard@ricebridge.com>
2014-07-10 18:39:08 +0100

    using explicit versions in package.json

--- data-entities/main.js
+++ data-entities/main.js
@@ -7,22 +7,16 @@ var seneca = require('seneca')()
 seneca.use( 'jsonfile-store', { folder:'json-data',  map:{'-/json/-':'*'}})
 seneca.use( 'level-store',    { folder:'level-data', map:{'-/level/-':'*'}})
 
-seneca.ready(function(err){
-  if(err) return console.error(err);
+seneca
+  .make$('json','foo',{propA:'val1',propB:'val2'})
+  .save$(function(err,json_foo){
+    if(err) return console.error(err);
+    console.log(''+json_foo)
 
-  var seneca = this
-
-  ;seneca
-    .make$('json','foo',{propA:'val1',propB:'val2'})
-    .save$(function(err,json_foo){
-      if(err) return console.error(err);
-      console.log(''+json_foo)
-
-  ;seneca
-    .make$('level','bar',{propA:'val3',propB:'val4'})
-    .save$(function(err,level_bar){
-      if(err) return console.error(err);
-      console.log(''+level_bar)
-
-  }) })
-})
+    seneca
+      .make$('level','bar',{propA:'val3',propB:'val4'})
+      .save$(function(err,level_bar){
+        if(err) return console.error(err);
+        console.log(''+level_bar)
+      }) 
+  })
